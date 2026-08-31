a757c0bad9aa3bce1b36afc3ac6d30725182fd44
Parvathy Santhini <parvathy@gfz.de>
2026-08-31 16:26:34 +0200

    Add GPS recipe with system test

---
+++ el_paso/recipes/gps/__init__.py
@@ -0,0 +1,11 @@
+
+from typing import Literal
+
+goes_satellite_literal = Literal[
+    "ns41", "ns48", "ns53", "ns54", "ns55", "ns56", "ns57", "ns58", "ns59",
+    "ns60", "ns61", "ns62", "ns63", "ns64", "ns65", "ns66", "ns67", "ns68",
+    "ns69", "ns70", "ns71", "ns72", "ns73", "ns74", "ns75", "ns76", "ns77",
+    "ns78", "ns79", "ns80", "ns81",
+]
+
+from el_paso.recipes.gps.process_gps import process_gps_data
