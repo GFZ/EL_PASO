ce6a8f85f32b8d7b9bf58f1288fad1bb1c6a4bdc
Sahil Jhawar <jhawar@gfz.de>
2026-09-08 17:34:33 +0200

    remove unused imports

--- el_paso/recipes/esa/process_ngrm_satellite.py
+++ el_paso/recipes/esa/process_ngrm_satellite.py
@@ -2,16 +2,13 @@
 # SPDX-FileContributor: Bernhard Haas
 #
 # SPDX-License-Identifier: Apache-2.0
-import argparse
 import logging
 import os
-import sys
 import typing
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Literal
 
-import dateutil
 import numpy as np
 from astropy import units as u
 from astropy.coordinates import GCRS, ITRS, CartesianRepresentation
